36a1909f73f5237c2db3243377cd9714598c225a
shivam_chaudhari19 <shivamsc7289@gmail.com>
2025-03-14 00:02:07 +0530

    service added

--- src/main/java/com/exam/examserver/service/UserService.java
+++ src/main/java/com/exam/examserver/service/UserService.java
@@ -1,4 +1,10 @@
 package com.exam.examserver.service;
 
+import com.exam.examserver.model.User;
+import com.exam.examserver.model.UserRole;
+
+import java.util.Set;
+
 public interface UserService {
+ public User createUser(User user, Set<UserRole> userRole) throws Exception;
 }
--- src/main/java/com/exam/examserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/exam/examserver/service/impl/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package com.exam.examserver.service.impl;
 
-public class UserServiceImpl {
+import com.exam.examserver.model.User;
+import com.exam.examserver.model.UserRole;
+import com.exam.examserver.repository.RoleRepository;
+import com.exam.examserver.repository.UserRepository;
+import com.exam.examserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+
+
+    @Override
+    public User createUser(User user, Set<UserRole> userRole) throws Exception {
+        User local=this.userRepository.findByUsername(user.getUsername());
+        if(local!=null){
+            System.out.println("username already exist!!!");
+            throw new Exception("User already present!!!!");
+        }else {
+            //rest code
+            for(UserRole ur:userRole){
+                roleRepository.save(ur.getRole());
+            }
+            user.getUserRoles().addAll(userRole);
+            local=this.userRepository.save(user);
+        }
+        return  local;
+    }
 }
